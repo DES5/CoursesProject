4954dab936987bc5f9334cc639ff22723c5296cd
DES5 <gargalasketci@gmail.com>
2021-10-28 01:41:23 +0300

    metatroph toy network apo class se arxeio me def

--- networkConnection.py
+++ networkConnection.py
@@ -6,34 +6,56 @@
     dbtype -> Τυπος βασης
 """
 import mysql.connector as c
-
-class networkConnection:
-
-    def __init__(self,dbname,username,userpass,dbtype):
-        self.dbname=dbname
-        self.username=username
-        self.userpass=userpass
-        self.dbtype=dbtype
-        try:
-            self.conn =c.connect(host=self.dbtype,user=self.username,password=self.userpass,database=self.dbname)
-            if self.conn.is_connected:
-                print(self.conn.connection_id)
-                print ("Succeful Connection with " + self.dbname)
-        except ConnectionError as erro:
-            print(erro)
+from mysql.connector import Error 
 
 
-    #funtion για την αποσυνδεση με την βαση
-    def dbDisconnect(self):
-        try:
-            if self.conn.is_connected:    
-                self.conn.close()
-                print("Succeful Disconnect from " + self.dbname)
-        except ConnectionError as erro:
-            print(erro) 
+
+    
+''' try:
+    self.conn =c.connect(host=self.dbtype,user=self.username,password=self.userpass,database=self.dbname)
+    if self.conn.is_connected:
+        print ("Succeful Connection with " + self.dbname)
+        print("Version: " + self.conn.get_server_info())
+except Error as e:
+    print("Error while try connecting with server ", e) '''
+
+
+#funtion για την αποσυνδεση με την βαση
+''' def dbDisconnect(self):
+try:
+    if self.conn.is_connected:    
+        self.conn.close()
+        print("Succeful Disconnect from " + self.dbname)
+except Error as e:
+    print("Error while try to disconnect from server", e) 
+ '''
+
+
+#function για την εισαγωγη δεδομενων στον πινακα Student
+def dbInsertDataAtStudent(hostname,username,userpass,db):
+    try:
+        conn=c.connect(host=hostname,user=username,password=userpass,database=db)
+        if conn.is_connected:
+            query = """INSERT INTO ExampleTable 
+                    VALUES ('hi',29)"""
+            cursor = conn.cursor()
+            cursor.execute(query)
+            conn.commit()
+            print("The Table has updated")
+    except Error as e:
+        print("Fail with updated the table: {}".format(e))
+        conn.rollback()
+    finally:
+        if conn.is_connected:
+            cursor.close()
+            conn.close()
+            print("Succeful disconect from server")
 
             
-test=networkConnection("Example","admin","Sg147896325!","localhost")
-test.dbDisconnect()
+
+
+
+dbInsertDataAtStudent("localhost","admin","Sg147896325!","Example")
+
 
 
